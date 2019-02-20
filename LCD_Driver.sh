102a347d42fd512146fad49751d8a758ed51ea39
ExpandingDev <tylersengia@gmail.com>
2019-02-19 23:20:05 -0500

    Added flags and documentation

--- LCD_Driver.sh
+++ LCD_Driver.sh
@@ -2,7 +2,8 @@
 
 # Many thanks to: https://www.raspberrypi-spy.co.uk/2012/07/16x2-lcd-module-control-using-python/
 
-#Below are GPIO pin mappings. Note, using BCM pin labels/numbering
+# Below are GPIO pin mappings. Note, using BCM pin labels/numbering
+# Change these values according to your configuration
 LCD_RS=25
 LCD_E=17
 LCD_D4=24
@@ -11,6 +12,8 @@ LCD_D6=22
 LCD_D7=18
 LCD_BACKLIGHT=8
 
+
+# These are not pins, these are constants, don't change these
 LCD_WIDTH=16
 LCD_CHR=1
 LCD_CMD=0
@@ -18,9 +21,26 @@ LCD_CMD=0
 LCD_LINE1=0x80
 LCD_LINE2=0xC0
 
-# The below variables are measured in seconds
+# The below variables are measured in seconds.
+# I have set them to 0 because bash execution usually takes well over milliseconds in time, but if you are having difficulties you can try changing the values. (try 0.0005 and/or 0.001)
 E_PULSE=0.0000
 E_DELAY=0.0000
+
+doClear=1
+
+usage() {
+	echo "
+	LCD_Driver\n
+	Usage:		./LCD_Driver.sh [-p ( 1 | 0 )] [-c ( 1 | 0 )] [TEXT]
+	
+	-p [VALUE]		Turn the LCD Backlight on (1) or off (0). If not specified, no changes are made to the current state of the LCD Backlight pin.
+	-c [VALUE]		If VALUE is 0, do not clear the display. If VALUE is 1, clear the display. Default is 1 (default is clear the display).
+	
+	Author: ExpandingDev
+	Issues, bugs, suggestions: https://github.com/ExpandingDev/Bash1602Driver
+	";
+	exit 0
+}
 
 toggle_enable() {
 	sleep $E_DELAY
@@ -30,6 +50,7 @@ toggle_enable() {
 	sleep $E_DELAY
 }
 
+# Sets all of the data pins to 0
 dataLow() {
 	gpio -g write $LCD_D4 0
 	gpio -g write $LCD_D5 0
@@ -37,7 +58,7 @@ dataLow() {
 	gpio -g write $LCD_D7 0
 }
 
-# The below function takes two args, the first is a string representing a hex number (0xFF) and the second is a 1 or 0 for mode (cmd or data mode)
+# The below function takes two args, the first is a number/byte and the second is a 1 or 0 for mode (cmd or data mode)
 writeByte() {
 	gpio -g write $LCD_RS $2
 
@@ -86,7 +107,6 @@ resetLCD() {
 	writeByte 0x06 $LCD_CMD
 	writeByte 0x0C $LCD_CMD
 	writeByte 0x28 $LCD_CMD
-	clearLCD
 	sleep $E_DELAY
 }
 
@@ -103,8 +123,33 @@ writeString() {
 }
 
 initPins
+setPower=2
+while getopts "p:c" o; do
+	case "${o}" in
+		p)
+			setPower=${OPTARG}
+			((setPower == 0 || setPower == 1)) || usage
+			;;
+		c)
+			doClear=1
+			;;
+		*)
+			usage
+			;;
+	esac
+done
+
 resetLCD
-lcdOn
+
+if [ $doClear -eq 1 ]; then
+	clearLCD
+fi
+
+if [ $setPower -eq 2 ]; then
+	#Do nothing as the user hasn't specified the flag
+else
+	gpio -g write $LCD_BACKLIGHT $setPower
+fi
 
 writeByte $LCD_LINE2 $LCD_CMD
 writeString "$1"
